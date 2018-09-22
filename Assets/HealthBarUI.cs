6478e526b79e84d9d2b3210ce0e5559ab825d53d
Laurence <101215@student.sae.edu.au>
2018-09-22 13:37:30 +0800

    health bars and damage dealing implemented

---
+++ Assets/HealthBarUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : MonoBehaviour
+{
+    private BaseUnit unit;
+
+    [SerializeField]
+    private RectTransform healthBar;
+
+	// Use this for initialization
+	void Start ()
+    {
+        unit = GetComponent<BaseUnit>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 newScale = new Vector3(unit.GetHealthRatio(), 1f, 1f);
+
+        healthBar.localScale = newScale;
+	}
+}
--- Assets/Scripts/BaseUnit.cs
+++ Assets/Scripts/BaseUnit.cs
@@ -4,34 +4,70 @@ using UnityEngine;
 
 public class BaseUnit : MonoBehaviour
 {
+    private UnitStates unitState = UnitStates.Move; // the current state of the unit
+
+
+
+    // THE COMMANDER OF THIS UNIT (PLAYER OR ENEMY)
     [Header("CHOOSE THE COMMANDER: ")]
     [SerializeField]
     private Commander commander = Commander.Player;
 
+
+
+
+    // RAYCASTING REFERENCES AND VARIABLES
     [Space(5)]
     [Header("RAYCAST DETAILS: ")]
     [SerializeField]
-    private Transform raycastOrigin;
+    private Transform raycastOrigin; // the origin of the raycast
 
     [SerializeField]
-    private float raycastRange = 8f;
+    private float raycastRange = 8f; // the maximum distance to cast the ray
 
     [SerializeField]
-    private float stoppingDistance = 3f, attackingRange = 1.5f;
+    private float stoppingDistance = 3f; // the distance in which a unit should stop moving
+
+    [SerializeField] 
+    private float attackingRange = 1.5f; // the distance in which the unit should start attacking respectively
+
+    private int stoppingLayerMask;
+    private int attackLayerMask; // a physics layer specially set up for the raycast that checks if an opposing unit is within attacking range
 
 
+
+
+
+
+    // ANIMATION REFERENCES AND VARIABLES
+    [Space(5)]
+    [Header("ANIMATION DETAILS: ")]
+    [SerializeField]
+    private AnimationClip attackAnimation; // the attack animation clip used
+
+    private float attackAnimLength; // the length of the attack animation
+
+    private Animator unitAnimator; // the units animator
+
+
+
+
+
+    // THE STATISTICS OF THIS UNIT
     [Space(5)]
     [Header("UNIT STATISTICS")]
     [Space(1)]
 
     [SerializeField]
-    private float health;
+    private int maxHealth;
+
+    private int currentHealth;
 
     [SerializeField]
     private float moveSpeed = 2f;
 
     [SerializeField]
-    private float damagePerHit;
+    private int damagePerHit;
 
     [SerializeField]
     private float hitPerSecond;
@@ -50,22 +86,16 @@ public class BaseUnit : MonoBehaviour
 
 
 
+    // ATTACKING VARIABLES
+    private bool attacking = false; // is the unit attacking?
+    private float attackTimer = 0f; // the timer that controls attacks
+    private float attackRate; // the time between attacks
+
+    private GameObject targetUnitGO = null; // the unit to attacks gameobject reference
+    private BaseUnit targetUnit = null; // the unit to attacks base unit script reference
 
 
 
-
-
-    private UnitStates unitState = UnitStates.Move;
-
-    private int attackLayerMask;
-
-    private float attackTimer = 0f;
-
-    private float attackRate;
-
-    private GameObject targetUnit = null;
-
-    private Animator unitAnimator;
 
 
 
@@ -74,14 +104,21 @@ public class BaseUnit : MonoBehaviour
 
     private void Awake()
     {
-        unitAnimator = GetComponent<Animator>();
+        unitAnimator = GetComponent<Animator>(); // grab the animator     
     }
+
+
+
 
 
 
 
     private void Start()
     {
+        stoppingLayerMask = 1 << 8; // the physics layer that stopping raycasts are used
+
+        // there is a physics layer for each commander type when attacking
+        // this checks which the commander type is and assigns the correct physics layer mask
         if (commander == Commander.Player)
         {
             attackLayerMask = 1 << 9;
@@ -91,16 +128,22 @@ public class BaseUnit : MonoBehaviour
             attackLayerMask = 1 << 10;
         }
 
-        attackRate = 1f / hitPerSecond;
+        attackRate = 1f / hitPerSecond; // calculate the time between attacks
+
+        currentHealth = maxHealth; // set the current health to full
+
+        attackAnimLength = attackAnimation.length; // grab the length of the attack animation
     }
 
 
 
 
 
-    // Update is called once per frame
+
+
     void Update ()
     {
+        // runs the appropriate method when the units state is changed
         switch(unitState)
         {
             case UnitStates.Move:
@@ -116,9 +159,17 @@ public class BaseUnit : MonoBehaviour
                 break;
         }
         
+        // if the unit is within its attacking range of an opposing unit
         if(CheckAttackingRange() == true)
         {
+            // try to attack
             Attack();
+        }  
+        // if the unit is not within attacking range of an opposing unit, set the target unit variables to null
+        else
+        {
+            targetUnitGO = null;
+            targetUnit = null;
         }
 	}
 
@@ -128,10 +179,13 @@ public class BaseUnit : MonoBehaviour
 
     private void Move()
     {
+        // start playing the move animation
         unitAnimator.SetTrigger("Move");
 
+        // move the unit in world space
         transform.position += transform.right * moveSpeed * Time.deltaTime;
 
+        // if there is another unit blocking this units path, change the state of this unit to idle (stops this unit moving)
         if(CheckStoppingRange() == true)
         {
             unitState = UnitStates.Idle;
@@ -143,8 +197,10 @@ public class BaseUnit : MonoBehaviour
 
     private void Idle()
     {
+        // start playing the idle animation
         unitAnimator.SetTrigger("Idle");
 
+        // if there is no other unit blocking this units path, change the state of this unit to move (starts this unit moving)
         if (CheckStoppingRange() == false)
         {
             unitState = UnitStates.Move;
@@ -153,29 +209,34 @@ public class BaseUnit : MonoBehaviour
 
 
 
-
-
-
     private void Attack()
     {
-        //if (commander == Commander.Player) Debug.Log("AttackTimer: " + attackTimer + "\t AttackRate: " + attackRate);
-
+        // start attacking the target unit
         if (attackTimer == 0)
         {
-            //if(commander == Commander.Player) Debug.Log("Attack!");
-            unitAnimator.SetTrigger("Attack");
+            unitAnimator.SetTrigger("Attack"); // trigger the attack animation
+            attacking = true; // switch the attacking boolean
         }
 
-       
+        // while currently attacking, when the attack animation has finished playing
+        if(attackTimer >= attackAnimLength && attacking == true)
+        {
+            attacking = false; // switch the attack boolean
+            targetUnit.SubtractHealth(damagePerHit); // subtract health from the target unit
+        }
 
+        // when the time between attacks has passed, set attack timer to 0 - triggering the next attack
         if (attackTimer >= attackRate)
         {
             attackTimer = 0f;
         }
+        // when the time between attacks has not yet passed
         else
         {
+            // if the attack hasn't just been triggered
             if(attackTimer != 0f)
             {
+                // check if in stopping range of a unit and set unit state to idle or move respectively
                 if (CheckStoppingRange())
                 {
                     unitState = UnitStates.Idle;
@@ -186,6 +247,7 @@ public class BaseUnit : MonoBehaviour
                 }
             }
 
+            // increment the attack timer
             attackTimer += Time.deltaTime;
         }
     }
@@ -200,41 +262,45 @@ public class BaseUnit : MonoBehaviour
 
 
 
-
+    // This method checks to see if the unit is within range to attack a target unit - if it is it returns true otherwise returns false
     private bool CheckAttackingRange()
     {
+        // determines if can attack
         bool attack = false;
 
+        // cast a 2D ray on the attack physics layer and get the hit information
         RaycastHit2D hit = Physics2D.Raycast(raycastOrigin.position, transform.right, attackingRange, attackLayerMask);
 
+        // if the raycast hit a collider
         if (hit.collider)
         {
-            Debug.DrawLine(raycastOrigin.position, hit.point, Color.green);
-
-            if(targetUnit == null)
+            // if a target unit has not yet been set
+            if(targetUnitGO == null)
             {
-                targetUnit = hit.transform.gameObject;
-                //Debug.Log("Hit the collidable object " + hit.collider.name);
+                // set the target units GO and BaseUnit script
+                targetUnitGO = hit.transform.gameObject;
+                targetUnit = targetUnitGO.GetComponent<BaseUnit>();
             }
 
-            if (targetUnit != null)
+            // if a target unit has been assigned
+            if (targetUnitGO != null)
             {
-                if (targetUnit.transform.tag == "Unit")
+                // if that target unit is a unit
+                if (targetUnitGO.transform.tag == "Unit")
                 {
+                    // if the distance between this unit and the target unit is smaller than the attackign range
                     if (hit.distance <= attackingRange)
                     {
-                        attack = true;
+                        attack = true; // this unit can attack!
                     }
 
                 }
             }
         }
+        // if the raycast did not hit a collider
         else
         {
-            if(targetUnit == null)
-            {
-                attack = false;
-            }
+            attack = false;
         }
 
         return attack;
@@ -244,22 +310,23 @@ public class BaseUnit : MonoBehaviour
 
 
 
+
+
+    // this method checks to see if this unit is within it's stopping (idle) range - if it is it returns true otherwise returns false
     private bool CheckStoppingRange()
     {
-        bool stop = false;
+        bool stop = false; // determines if can attack
 
-        int layerMask = 1 << 8;
+        // cast a 2D ray on the stopping physics layer and get the hit information
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin.position, transform.right, raycastRange, stoppingLayerMask);
 
-        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin.position, transform.right, raycastRange, layerMask);
-
+        // if the raycast hit a collider
         if (hit.collider)
         {
-            //Debug.DrawLine(raycastOrigin.position, hit.point, Color.red);
-
+            // if that collider was a unit
             if (hit.transform.tag == "Unit")
             {
-                //Debug.Log("Hit the collidable object " + hit.collider.name);
-
+                // if that collider is within the stopping distance of this unit - this unit should stop moving
                 if (hit.distance <= stoppingDistance)
                 {
                     stop = true;
@@ -273,14 +340,34 @@ public class BaseUnit : MonoBehaviour
 
 
 
-
+    // sets the commander of this unit
     public void SetCommander(Commander inCommander)
     {
         commander = inCommander;
     }
 
+    // grabs the commander of this unit
     public Commander GetCommander()
     {
         return commander;
     }
+
+
+    // calculates and returns the ratio of currenthealth to health - should always return a number between 0 & 1 (inclusive)
+    public float GetHealthRatio()
+    {   
+        return (float)currentHealth / (float)maxHealth;
+    }
+
+    // subtracts health from this unit and caps the minimum to 0
+    public void SubtractHealth(int inNum)
+    {
+        currentHealth -= inNum;
+
+        if (currentHealth <= 0) currentHealth = 0;
+
+        Debug.Log("HEALTH: " + currentHealth);
+    }
+
+    
 }
