37c61a5615fa75b5b97a56bea332fa55a372ed01
Laurence <40289918+protodroid-IB@users.noreply.github.com>
2018-10-01 16:21:40 +0800

    implemented speed increase

---
+++ Assets/FastForward.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastForward : MonoBehaviour
+{
+    [SerializeField]
+    private float increasedGameSpeed = 2f;
+
+    public void SpeedUp()
+    {
+        Time.timeScale = increasedGameSpeed;
+    }
+
+    public void SpeedNormal()
+    {
+        Time.timeScale = 1.0f;
+    }
+	
+}
