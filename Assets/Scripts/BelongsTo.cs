7c6206c0bcf060cd8c9a9ddc3b9690f33e9c8935
Laurence <40289918+protodroid-IB@users.noreply.github.com>
2018-10-01 13:07:46 +0800

    all death explosions implemented for all units

---
+++ Assets/Scripts/BelongsTo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BelongsTo : MonoBehaviour
+{
+    public Commander commander = Commander.Player;
+
+	
+}
