e0ebe8510be64e02bca1e7dee17abeba081b89d5
Laurence <101215@student.sae.edu.au>
2018-10-05 12:07:04 +0800

    commented some more

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+/* 
+    SCRIPT AUTHOR/S:
+    William Roberts with additions by Laurence Valentini
+
+    SCRIPT DESCRIPTION: 
+    This script should be used to easily find and play specific sound effects. It should stay persistent throughout all scenes.
+
+    To use this:
+    // AudioManager audiomanager;
+    // in start method { audiomanager = AudioManager.instance; }
+    // to play sound audioManager.PlaySound("Sound Name")
+    // or to play sound from a specific audiosource audioManager.PlaySound("Sound Name", audiosource)
+
+*/
+
 public class AudioManager : MonoBehaviour
 {
-    // AudioManager audiomanager;
-    // in start audiomanager = AudioManager.instance;
-    // to play sound audioManager.PlaySound("Sound Name")
-    public static AudioManager instance;
+
+    public static AudioManager instance; // a static instance of this script
 
     [SerializeField]
+    Sound[] sound; // an array of sound class which is a container for sound effects and their audio properties
 
-    Sound[] sound;
+    public bool isMuted = false; // is the sound muted
 
-    public bool isMuted = false;
+
+
+
 
 	// Use this for initialization
 	void Awake ()
     {
+        // this block of code ensures that the object does not destroy itself or become duplicated
         DontDestroyOnLoad(gameObject);
+
 		if (instance == null)
         {
             instance = this;
@@ -30,8 +48,12 @@ public class AudioManager : MonoBehaviour
         }
 	}
 
+
+
+
     void Start()
     {
+        // go through each sound in the sound array and greate a gameobject parented to the audio manager with an audio source
         for (int i = 0; i < sound.Length; i++)
         {
             GameObject _go = new GameObject("Sound_" + i + "_" + sound[i].clipName);
@@ -39,9 +61,13 @@ public class AudioManager : MonoBehaviour
             sound[i].SetSource(_go.AddComponent<AudioSource>());
         }
 
+        // play the theme music
         PlaySound("Theme");
     }
     
+
+
+    // this method finds which sound is to be played and plays it using the created audio source
     public void PlaySound(string _name)
     {
 		for (int i = 0; i < sound.Length; i++)
@@ -54,7 +80,7 @@ public class AudioManager : MonoBehaviour
         }
 	}
 
-
+    // this method finds which sound is to be played and plays it using an already existing audio source that is passed in as a parameter
     public void PlaySound(string _name, AudioSource _audioSource)
     {
         for (int i = 0; i < sound.Length; i++)
